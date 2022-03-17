3d18c2baa068ac4b1d39c4f9be97b206f7f0822c
Debed666 <100848478+Debed666@users.noreply.github.com>
2022-03-17 17:42:41 +0200

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -12,24 +12,42 @@ resEternalBlue = []
 resSmbghost = []
 
 for item in jsonData:
-    for port in item["ports"]:
-        hostname = "eltons-dev"
-        username = "elton"
-        if exploitEternalblue(item["ip"], port):
-            resEternalBlue.append({
-                "ip": item["ip"],
-                "port": port
-            })
-        if exploitBluekeep(item["ip"], port):
-            resBlueKeep.append({
-                "ip": item["ip"],
-                "port": port
-            })
-        if exploitSmbghost(item["ip"]):
-            resSmbghost.append({
-                "ip": item["ip"],
-                "port": port
-            })
+    hostname = "eltons-dev"
+    username = "elton"
+    port = 445
+    if exploitEternalblue(item["ip"], port):
+        resEternalBlue.append({
+            "ip": item["ip"],
+            "port": port
+        })
+    if exploitBluekeep(item["ip"], port, hostname, username):
+        resBlueKeep.append({
+            "ip": item["ip"],
+            "port": port
+        })
+    if exploitSmbghost(item["ip"]):
+        resSmbghost.append({
+            "ip": item["ip"],
+            "port": port
+        })
+
+    port = 3389
+    if exploitEternalblue(item["ip"], port):
+        resEternalBlue.append({
+            "ip": item["ip"],
+            "port": port
+        })
+    if exploitBluekeep(item["ip"], port, hostname, username):
+        resBlueKeep.append({
+            "ip": item["ip"],
+            "port": port
+        })
+    if exploitSmbghost(item["ip"]):
+        resSmbghost.append({
+            "ip": item["ip"],
+            "port": port
+        })
+        
 
 resBlueKeepJson = json.dumps(resBlueKeep)
 jsonFileNameBluekeep = fileName.split(".")[0] + "BlueKeep.json"
@@ -40,8 +58,3 @@ resEternalblueJson = json.dumps(resEternalBlue)
 jsonFileNameEternalBlue = fileName.split(".")[0] + "Eternalblue.json"
 open(jsonFileNameEternalBlue, "w").write(resEternalblueJson)
 print(str(len(resEternalBlue)) + " items has eternalblue vulnerability, added to file " + jsonFileNameEternalBlue)
-
-resSmbghostJson = json.dumps(resSmbghost)
-jsonFileNameEternalBlue = fileName.split(".")[0] + "Smbghost.json"
-open(jsonFileNameEternalBlue, "w").write(resSmbghostJson)
-print(str(len(resSmbghostJson)) + " items has smbghost vulnerability, added to file " + jsonFileNameEternalBlue)
